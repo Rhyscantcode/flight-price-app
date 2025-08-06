47e8922ebdf068cb8a6b4ad4af88599780dc6c45
Rhys Ashwell <rhyssashwell@gmail.com>
2025-08-06 02:52:44 +0000

    it doesnt look the same at all

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -14,7 +14,7 @@ export function Logo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
       {...props}
     >
       <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-      <path d="M17.5 9.5a4.6 4.6 0 0 0-8.24 3" />
+      <path d="M14.5 9.5a4.6 4.6 0 0 0-8.24 3" />
       <path d="M14.5 15.5a3.6 3.6 0 0 1-5.73-2" />
     </svg>
   );
